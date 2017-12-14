4918d229406ee543c4398360acdd37d38fd4ec33
c20em <32170400+c20em@users.noreply.github.com>
2017-12-14 16:02:29 -0500

    Beginning Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmNomNomNom/BaseChassis.java
@@ -0,0 +1,178 @@
+package org.firstinspires.ftc.teamcode.OmNomNomNom;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by student on 12/14/17.
+ */
+
+@TeleOp(name="Skipper Teleop LEFT STICK and DPAD", group="Linear Opmode")
+public class BaseChassis extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontLeftDrive = null;
+    private DcMotor FrontRightDrive = null;
+    private DcMotor BackLeftDrive = null;
+    private DcMotor BackRightDrive = null;
+
+    //static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   75;
+
+    // Define class members
+    double strafepower = 1;
+
+    controllerPos previousDrive = controllerPos.ZERO;
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        FrontLeftDrive = hardwareMap.get(DcMotor.class, "front_left");
+        FrontRightDrive = hardwareMap.get(DcMotor.class, "front_right");
+        BackLeftDrive = hardwareMap.get(DcMotor.class, "back_left");
+        BackRightDrive = hardwareMap.get(DcMotor.class, "back_right");
+
+        FrontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        BackLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        BackRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        FrontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            telemetry.update();
+
+            moveRobot();
+
+            sleep(CYCLE_MS);
+            idle();
+        }
+    }
+    public enum controllerPos {
+        STRAFE_RIGHT, STRAFE_LEFT, DRIVE_FOWARD, DRIVE_BACK, TURN_RIGHT, TURN_LEFT, SLOW_MODE, ZERO;
+    }
+
+    //DRIVING CONTROL
+    public void moveRobot() {
+        double drive = gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
+
+        if(drive > 0.25 && (previousDrive == controllerPos.DRIVE_FOWARD || previousDrive == controllerPos.ZERO)) {
+            previousDrive = controllerPos.DRIVE_FOWARD;
+            Drive(drive);
+        } else if(drive < -0.25 && (previousDrive == controllerPos.DRIVE_BACK || previousDrive == controllerPos.ZERO)) {
+            previousDrive = controllerPos.DRIVE_BACK;
+            Drive(drive);
+        } else if(gamepad1.dpad_right && (previousDrive == controllerPos.STRAFE_RIGHT || previousDrive == controllerPos.ZERO)) {
+            previousDrive = controllerPos.STRAFE_RIGHT;
+            Strafe(1);
+        } else if(gamepad1.dpad_left && (previousDrive == controllerPos.STRAFE_LEFT || previousDrive == controllerPos.ZERO)) {
+            previousDrive = controllerPos.STRAFE_LEFT;
+            Strafe(-1);
+        }  else if(turn > 0.25 &&(previousDrive == controllerPos.TURN_RIGHT || previousDrive == controllerPos.ZERO)){
+            previousDrive = controllerPos.TURN_RIGHT;
+            turn(turn);
+        } else if(turn < -0.25 &&(previousDrive == controllerPos.TURN_LEFT || previousDrive == controllerPos.ZERO)){
+            previousDrive = controllerPos.TURN_LEFT;
+            turn(turn);
+        }  else if(gamepad1.right_trigger > 0.2 && (previousDrive == controllerPos.SLOW_MODE || previousDrive == controllerPos.ZERO)) {
+            previousDrive = controllerPos.SLOW_MODE;
+            Drive(-0.4);
+        }
+        else {
+            previousDrive = controllerPos.ZERO;
+            FrontLeftDrive.setPower(0);
+            BackLeftDrive.setPower(0);
+            FrontRightDrive.setPower(0);
+            BackRightDrive.setPower(0);
+        }
+
+    }
+
+    //STRAFING CONTROL
+    public void Strafe(int strafedirection) {
+
+        double FRpower = strafedirection * strafepower;
+        double BLpower = strafedirection * strafepower;
+        double BRpower = -1 * strafedirection * strafepower;
+        double FLpower = -1 * strafedirection * strafepower ;
+
+        FLpower = Range.clip(FLpower, -1.0, 1.0) ;
+        BRpower = Range.clip(BRpower, -1.0, 1.0) ;
+        BLpower = Range.clip(BLpower, -1.0, 1.0) ;
+        FRpower = Range.clip(FRpower, -1.0, 1.0) ;
+
+        readjustMotorPower(FRpower);
+        readjustMotorPower(BRpower);
+        readjustMotorPower(FLpower);
+        readjustMotorPower(BLpower);
+
+        FrontLeftDrive.setPower(FLpower);
+        BackLeftDrive.setPower(BLpower);
+        FrontRightDrive.setPower(FRpower);
+        BackRightDrive.setPower(BRpower);
+
+    }
+
+    //DRIVING FOWARADS/BACKWARDS/TURNING
+    public void Drive(double drive) {
+        double drivePower = drive;
+
+        drivePower = readjustMotorPower(drivePower);
+        drivePower = Range.clip(drivePower, -1.0, 1.0);
+
+        FrontLeftDrive.setPower(drivePower);
+        BackLeftDrive.setPower(drivePower);
+        FrontRightDrive.setPower(drivePower);
+        BackRightDrive.setPower(drivePower);
+
+        telemetry.addData("Motors", "drive power (%.2f)", drivePower);
+        telemetry.update();
+    }
+    public void turn(double turn){
+        double Rpower = turn;
+        double Lpower = -turn;
+
+        Rpower = readjustMotorPower(Rpower);
+        Lpower = readjustMotorPower(Lpower);
+
+        Rpower = Range.clip(Rpower, -1.0, 1.0);
+        Lpower = Range.clip(Lpower, -1.0, 1.0);
+
+        FrontLeftDrive.setPower(Lpower);
+        BackLeftDrive.setPower(Lpower);
+        FrontRightDrive.setPower(Rpower);
+        BackRightDrive.setPower(Rpower);
+    }
+
+
+    //KEEPS MOTORS FROM STALLING
+    public double readjustMotorPower(double motorPower) {
+        if (Math.abs(motorPower) >= 0.3) {
+            return motorPower;
+        } else {
+            return 0;
+        }
+    }
+}
+
+
+
+
