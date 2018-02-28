c2b7d3e5259bf41e7012ac87082f71444d5b1652
acohen <c19ac@daltong.org>
2018-02-27 20:24:44 -0500

    started new (fuego) autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuegoAutos/Blue1Fuego.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.FuegoAutos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+/**
+ * peep FuegoRobot.java
+ */
+
+@Autonomous(name="Blue 1 Fuego", group="Autonomous")
+
+public class Blue1Fuego extends LinearOpMode {
+    FuegoRobot omnom;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        omnom = new FuegoRobot();
+        omnom.initPositions();
+        waitForStart();
+
+        telemetry.addLine("made it this far");
+        telemetry.update();
+
+        //get vuforia column
+        omnom.column = omnom.getPicto();
+
+        //run jewel stuff
+        omnom.jewel(true);
+        sleep(600);
+
+        //MOVE TO THE CORRECT COLUMN
+        if (omnom.column == RelicRecoveryVuMark.CENTER || omnom.column == RelicRecoveryVuMark.UNKNOWN) {
+            omnom.driveforTime(0.5, 1200); //fill w center value
+        } else if (omnom.column == RelicRecoveryVuMark.LEFT) {
+            omnom.driveforTime(0.5, 1200); //fill w left value
+        } else if (omnom.column == RelicRecoveryVuMark.RIGHT) {
+            omnom.driveforTime(0.5, 1200);//fill w right value
+        } else  omnom.driveforTime(0.5, 1200);
+
+        sleep(200);
+        omnom.turnAngleCW(90);
+        sleep(300);
+        omnom.driveforTime(.7, 800);
+
+        //place the first glyph
+        omnom.placeGlyphSequence();
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuegoAutos/FuegoRobot.java
@@ -0,0 +1,384 @@
+package org.firstinspires.ftc.teamcode.FuegoAutos;
+
+import android.app.Activity;
+import android.view.View;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.Locale;
+
+/*
+    welcome to the holy grail for all fuego autos
+ */
+
+public class FuegoRobot {
+
+    //COLOR SENSOR VARIABLES
+    float hsvValues[] = {0F, 0F, 0F};
+    final float values[] = hsvValues;
+    final double SCALE_FACTOR = 255;
+    DistanceSensor sensorDistance;
+
+
+    //SYSTEM VARIABLES
+    ElapsedTime clock = new ElapsedTime();
+    RelicRecoveryVuMark column = RelicRecoveryVuMark.UNKNOWN;
+    private LinearOpMode opMode;
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //MOTOR VARIABLES AND THINGS
+    DcMotor FrontLeftDrive = null;
+    DcMotor FrontRightDrive = null;
+    DcMotor BackLeftDrive = null;
+    DcMotor BackRightDrive = null;
+    DcMotor lift = null;
+    DcMotor NomNomNom = null;
+    Servo rightBoxServo = null;
+    Servo leftBoxServo = null;
+    Servo liftIn = null;
+    Servo elbowServo = null;
+    Servo wallServo = null;
+    CRServo pushBackServoLeft = null;
+    CRServo pushBackServoRight = null;
+    ColorSensor colorSensor;
+    Servo jewelServo;
+
+    //POSITION VARIABLES
+    static final double JEWEL_DOWN_POS = 0.0;
+    static final double JEWEL_MID_POS = 0.2;
+    static final double JEWEL_UP_POS = 0.6;
+
+    static double BOX_RIGHT_DOWN = .84;
+    static double BOX_LEFT_DOWN = .1;
+    static final double BOX_RIGHT_UP = .34;
+    static final double BOX_LEFT_UP = .61;
+    static final double ELBOW_UP = .1;
+
+    double nomPower = 0.95;
+    boolean canSeeJewel = false;
+
+    //GYRO THINGS
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+
+    public void init(LinearOpMode o){
+        //SYSTEM THINGS
+        opMode = o;
+        hardwareMap = opMode.hardwareMap;
+        telemetry = opMode.telemetry;
+
+        //HARDWARE THINGS
+        FrontLeftDrive = hardwareMap.get(DcMotor.class, "front_left");
+        FrontRightDrive = hardwareMap.get(DcMotor.class, "front_right");
+        BackLeftDrive = hardwareMap.get(DcMotor.class, "back_left");
+        BackRightDrive = hardwareMap.get(DcMotor.class, "back_right");
+        colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
+        rightBoxServo = hardwareMap.get(Servo.class, "right_box_servo");
+        leftBoxServo = hardwareMap.get(Servo.class, "left_box_servo");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        NomNomNom = hardwareMap.get(DcMotor.class, "nom");
+        liftIn = hardwareMap.get(Servo.class, "lift_in");
+        elbowServo = hardwareMap.get(Servo.class, "elbow_servo");
+        wallServo = hardwareMap.get(Servo.class, "wall_servo");
+        pushBackServoRight = hardwareMap.get(CRServo.class, "push_back_servo_right");
+        pushBackServoLeft = hardwareMap.get(CRServo.class, "push_back_servo_left");
+        jewelServo = hardwareMap.get(Servo.class, "jewel_servo");
+
+        FrontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        BackLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        BackRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        FrontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        NomNomNom.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        NomNomNom.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //GYRO THINGS
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        //COLORSENSOR MAYBE VUFORIA THINGS
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "colorSensor");
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+    }
+
+    public void idle(){
+        opMode.idle();
+    }
+
+    public boolean opModeIsActive(){
+        return opMode.opModeIsActive();
+    }
+
+    public double getRuntime(){
+        return opMode.getRuntime();
+    }
+
+    public void initPositions(){
+        liftIn.setPosition(.9);             //Relic Blocker
+        elbowServo.setPosition(ELBOW_UP);    //Relic arm up
+        delay(1000);                         //breif pause so that wall servo does not interfere with relic arm release
+        wallServo.setPosition(.78);          //Wall servo out
+    }
+
+    public void strafeforTime(double power, int time) throws InterruptedException {
+        strafe(power);
+        delay(time);
+        StopDriving();
+    }
+
+    public void strafe(double power) {
+        FrontLeftDrive.setPower(power);
+        BackLeftDrive.setPower(-power);
+        FrontRightDrive.setPower(-power);
+        BackRightDrive.setPower(power);
+    }
+
+    public void turn(double power, int time) throws InterruptedException{
+        turn(power);
+        delay(time);
+        StopDriving();
+    }
+
+    public void driveforTime(double power, int time){
+        drive(power);
+        delay(time);
+        StopDriving();
+    }
+
+    public void turn(double turn){
+        FrontLeftDrive.setPower(turn);
+        BackLeftDrive.setPower(turn);
+        FrontRightDrive.setPower(-turn);
+        BackRightDrive.setPower(-turn);
+    }
+
+    public void StopDriving(){
+        FrontLeftDrive.setPower(0);
+        BackLeftDrive.setPower(0);
+        FrontRightDrive.setPower(0);
+        BackRightDrive.setPower(0);
+    }
+
+    public void drive(double power) {
+        FrontLeftDrive.setPower(power); //THIS IS DRIVING BACKWARDS
+        BackLeftDrive.setPower(power);
+        FrontRightDrive.setPower(power);
+        BackRightDrive.setPower(power);
+        telemetry.addData("Motors", "drive power (%.2f)", power);
+    }
+
+    public void lift(double liftpower, int time) throws InterruptedException{
+        lift.setPower(liftpower);
+        delay(time);
+        lift.setPower(0);
+    }
+
+    public void flipOut(){
+        leftBoxServo.setPosition(BOX_LEFT_UP);
+        rightBoxServo.setPosition(BOX_RIGHT_UP);
+    }
+
+    public void flipIn(){
+        leftBoxServo.setPosition(BOX_LEFT_DOWN);
+        rightBoxServo.setPosition(BOX_RIGHT_DOWN);
+    }
+
+    public void nomforTime(int time) throws InterruptedException {
+        nom();
+        delay(time);
+        stopNom();
+    }
+
+    public void nom() {
+        NomNomNom.setPower(nomPower);
+        pushBackServoLeft.setPower(-1);
+        pushBackServoRight.setPower(1);
+    }
+
+    public void stopNom() {
+        NomNomNom.setPower(0);
+        pushBackServoLeft.setPower(0);
+        pushBackServoRight.setPower(0);
+    }
+
+    public void nomDrive(double power) {
+        nom();
+        drive(power);
+    }
+
+    public void nomDriveForTime(double power, int time)throws InterruptedException{
+        nomDrive(power);
+        delay(time);
+        StopDriving();
+        stopNom();
+    }
+
+    public void placeGlyphSequence(){
+        flipOut();
+        delay(300);
+        driveforTime(-.4, 800);
+        flipIn();
+        driveforTime(.4, 800);
+        flipOut();
+        delay(300);
+        driveforTime(-.4, 800);
+    }
+
+    public void jewel(boolean teamBlue) throws InterruptedException {
+        boolean jewelBlue;
+        jewelServo.setPosition(JEWEL_DOWN_POS);
+        //read color
+        int red = 0;
+        int blue = 0;
+        for (int i = 0; i < 40; i++) {
+            if (colorSensor.red() > colorSensor.blue()) red++;
+            if (colorSensor.red() < colorSensor.blue()) blue++;
+            telemetry.update();
+        }
+
+        //decide which color we see
+        if(blue>red){
+            jewelBlue= true;
+            telemetry.addData("blueWins!", blue);
+        } else {
+            jewelBlue=false;
+            telemetry.addData("redWins!", red);
+        }
+
+        //knock off correct jewel
+        if(jewelBlue){
+            if(teamBlue) turnAngleCW(15);
+            if(!teamBlue) turnAngleCCW(15);
+        } else if (!jewelBlue){
+            if(teamBlue) turnAngleCCW(15);
+            if(!teamBlue) turnAngleCW(15);
+        }
+        delay(200);
+
+        //turn back
+        if(!jewelBlue){
+            if(teamBlue) turnAngleCW(15);
+            if(!teamBlue) turnAngleCCW(15);
+        } else if (jewelBlue){
+            if(teamBlue) turnAngleCCW(15);
+            if(!teamBlue) turnAngleCW(15);
+        }
+    }
+
+    public void delay(int time) {
+        double delayStartTime = clock.milliseconds();
+        while (clock.milliseconds() - delayStartTime < time) {
+        }
+    }
+
+    public RelicRecoveryVuMark getPicto() { //function to figure out which column it is
+        OpenGLMatrix lastLocation = null;
+
+        VuforiaLocalizer vuforia;
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = "Aeba4Qn/////AAAAGahNOxzreUE8nItPWzsrOlF7uoyrR/qbjue3kUmhxZcfZMSd5MRyEY+3uEoVA+gpQGz5KyP3wEjBxSOAb4+FBYMZ+QblFU4byMG4+aiI+GeeBA+RatQXVzSduRBdniCW4qehTnwS204KTUMXg1ioPvUlbYQmqM5aPMx/2xnYN1b+htNBWV0Bc8Vkyspa0NNgz7PzF1gozlCIc9FgzbzNYoOMhqSG+jhKf47SZQxD6iEAtj5iAkWBvJwFDLr/EfDfPr3BIA6Cpb4xaDc0t4Iz5wJ/p4oLRiEJaLoE/noCcWFjLmPcw9ccwYXThtjC+7u0DsMX+r+1dMikBCZCWWkLzEyjWzy3pOOR3exNRYGZ0vzr";
+
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK; //look through back camera
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        relicTrackables.activate();
+
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        double vuStartTime = getRuntime();
+        while (getRuntime() - vuStartTime < 3) {
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        }
+
+        return vuMark;
+    }
+
+    double currentAngle() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle);
+    }
+
+    public void turnAngleCW(double angle) {
+        double startingAngle = currentAngle();
+        while(getAngleDiff(startingAngle,currentAngle())<angle){
+            double difference = ((angle-getAngleDiff(startingAngle,currentAngle()))/(angle*4));
+            telemetry.addData("difference",difference);
+            telemetry.update();
+            if(difference > .15) turn(difference);
+            else turn(.15);
+        }
+        StopDriving();
+    }
+
+    public void turnAngleCCW(double angle) {
+        double startingAngle = currentAngle();
+        while(getAngleDiff(startingAngle,currentAngle())<angle){
+            double difference = ((angle-getAngleDiff(startingAngle,currentAngle()))/(angle*4));
+            telemetry.addData("difference",difference);
+            telemetry.update();
+            if(difference > .15) turn(-difference);
+            else turn(-.15);
+        }
+        StopDriving();
+    }
+
+    public double getAngleDiff(double angle1, double angle2) {
+        if(Math.abs(angle1 - angle2) < 180.0)
+            return Math.abs(angle1-angle2);
+        else if(angle1 > angle2)
+        {
+            angle1 -= 360;
+            return Math.abs(angle2-angle1);
+        }
+        else
+        {
+            angle2 -= 360;
+            return Math.abs(angle1-angle2);
+        }
+    }
+
+}
